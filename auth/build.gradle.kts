d54075c3c30fe587b59fdc74e0718177fc8837cc
salpt <salpt@salpts-MacBook-Pro.local>
2024-12-04 21:35:46 +0800

    upgrade to spring 3.4.0

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    id("buildlogic.kotlin-application-conventions")
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("buildlogic.kotlin-spring-conventions")
 }
 
 group = "com.example"
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,4 +14,7 @@ repositories {
 
 dependencies {
     implementation(libs.kotlin.gradle.plugin)
+    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:1.9.22")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.4.0")
+    implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.6")
 }
